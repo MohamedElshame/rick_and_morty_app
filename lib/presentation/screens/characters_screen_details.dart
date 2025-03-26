88f23406f0771cec7b0597641d3e07c3a162712c
Mohamed Elhshamy <mohamed20031023@gmail.com>
2025-03-27 00:19:05 +0200

    Blocbuilder

--- lib/presentation/screens/characters_screen_details.dart
+++ lib/presentation/screens/characters_screen_details.dart
@@ -1,33 +1,132 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty_app/business_logic/cubit/characters_cubit.dart';
+import 'package:rick_and_morty_app/constants/colors.dart';
+import 'package:rick_and_morty_app/data/models/characters.dart';
+import 'package:rick_and_morty_app/presentation/widgets/character_item.dart';
 
-class CharacterScreenDetails extends StatelessWidget{
+class CharacterScreenDetails extends StatefulWidget{
 
   const CharacterScreenDetails({super.key});
+
+  @override
+  State<CharacterScreenDetails> createState() => _CharacterScreenDetailsState();
+}
+
+class _CharacterScreenDetailsState extends State<CharacterScreenDetails> {
+ 
+
+ late List<Character> ?allcharacters;
+ 
+ @override
+  void initState() {
+   
+    super.initState();
+
+allcharacters = 
+BlocProvider.of<CharactersCubit>(context).getAllcharacters();
+
+  }
+ Widget buildBlocWidget(){
+
+return BlocBuilder< CharactersCubit, CharactersState>
+(builder: (context, state )
+
+{
+if (state is CharactersLoaded){
+
+allcharacters =state.characters;
+
+
+return buildLoadedListWidgets();
+
+}
+else{
+return showLoadingIndicator();
+
+}
+},
+
+);
+
+
+
+ }
+Widget showLoadingIndicator(){
+return Center(
+
+child:  CircularProgressIndicator(
+  color: Mycolors.Myblue,
+),
+
+
+);
+
+
+
+}
+
+
+ Widget buildLoadedListWidgets(){
+return SingleChildScrollView(
+child: Container(
+
+color: Mycolors.MyBeige,
+child: Column(children: [
+buildCharactersList(),
+
+
+],),
+
+
+
+
+),
+
+);
+
+
+ }
+
+Widget buildCharactersList(){
+
+return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+childAspectRatio: 2/3,
+crossAxisSpacing: 1,
+mainAxisSpacing: 1,
+),
+shrinkWrap: true,
+physics: ClampingScrollPhysics()
+, itemBuilder:(ctx,index){
+//todo not done yet
+return CharacterItem();
+
+
+
+}
+
+
+,);
+
+
+
+}
+
   @override
  
   Widget build(BuildContext context) {
    return Scaffold(
+backgroundColor: Mycolors.MyPurple,
 
-body: Column(
-children: [
-Text("hi")
-
+body: buildBlocWidget(),
 
 
-],
 
-
-),
 
 
 
    );
   }
-
-
-
-
-
 }
---
+++ lib/presentation/widgets/character_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CharacterItem extends StatelessWidget{
+
+  
+ const CharacterItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold();
+  }
+
+
+}
