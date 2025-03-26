99641f32a0d561ef2b2e03c72490acbc3926a19f
Mohamed Elhshamy <mohamed20031023@gmail.com>
2025-03-26 03:20:48 +0200

    adding cubit

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:rick_and_morty_app/data/models/characters.dart';
+import 'package:rick_and_morty_app/data/repo/characters_repository.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+
+  final CharactersRepository charactersRepository;
+  late List<Character> characters ;
+  CharactersCubit(this.charactersRepository) : super(CharactersInitial());
+
+List<Character> ?getAllcharacters(){
+  charactersRepository.getAllcharacters().then((characters){
+emit(CharactersLoaded(characters));
+this.characters=characters;
+
+
+  });
+
+return characters;
+
+
+}
+
+
+
 }
--- lib/data/repo/characters_repository.dart
+++ lib/data/repo/characters_repository.dart
@@ -9,7 +9,7 @@ CharactersRepository(this.charactersApi);
 //? repo passes the data to cubit same as web services 
 //! Used the same object name as in the other folder  
 //? since they have nearly the same functionality.
-Future<List<dynamic>> getAllcharacters() async{
+Future<List<Character>> getAllcharacters() async{
 final characters=await charactersApi.getAllcharacters();
 
 return characters.map((character)=>Character.fromJson(character)).toList();
